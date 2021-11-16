d8083e92ab86ccc3fc6da505d9519eab96d26611
espilce@gmail.com <espilce@gmail.com>
2021-11-16 10:27:06 +0200

    1 ітерація

---
+++ encyclopedia/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.forms.widgets import HiddenInput
+from . import util
+
+
+class CreateNewPageForm(forms.Form):
+    name_page = forms.CharField()
+    content = forms.CharField(widget=forms.Textarea)
+
+
+class EditArticle(forms.Form):
+    content = forms.CharField(widget=forms.Textarea)
+    title = forms.CharField(widget=forms.HiddenInput())
--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+import encyclopedia
+
+app_name = "encyclopedia"
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("create", views.create, name="create"),
+    path("random", views.random, name="random"),
+    path("edit/", views.edit, name="edit"),
+    path("wiki/<str:title>", views.article, name="article")
 ]
